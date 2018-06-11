73d8fbf23cbf9c78528a737f213dbc2d729c7ad9
Fabio <fabioaab@gmail.com>
2018-06-11 17:13:59 -0300

    First commit

---
+++ src/main/java/br/gov/rj/tce/jarvis/JarvisApplication.java
@@ -0,0 +1,12 @@
+package br.gov.rj.tce.jarvis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarvisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JarvisApplication.class, args);
+	}
+}
---
+++ src/test/java/br/gov/rj/tce/jarvis/JarvisApplicationTests.java
@@ -0,0 +1,14 @@
+package br.gov.rj.tce.jarvis;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class JarvisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
